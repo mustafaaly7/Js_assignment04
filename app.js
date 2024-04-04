9ed682198fcc193309e6550a4388c961c01398b0
Mustafa Ali <mustafaaaly27@gmail.com>
2024-04-04 23:38:38 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// -----> Question 1 
+
+// var firstName = prompt("Enter Your First Name")
+// var lastName = prompt("Enter Your Last Name")
+// var fullName = firstName+ "   " + lastName ;
+// console.log(fullName);
+// alert("Welcome \n" + ""+ fullName)
+
+
+
+// -----> Question 2 
+
+// var phone = prompt("What's Your Favourite phone ??");
+
+// alert("Your Favourite phone: " + phone + "  \n " + " Length of String : " + phone.length);
+
+
+// -----> Q3
+
+var string = "Pakistan" ;
+// alert(string.indexOf("n"))
+var index =prompt("what alphabet index u want??")
+alert(string.indexOf(index))
